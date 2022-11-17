0784eb86f03b8f2656befa4cc0144d15472394b5
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-11-17 10:41:25 -0300

    components to merge maded

--- frontend/cabininn/src/componentsToMerge.js
+++ frontend/cabininn/src/componentsToMerge.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
 import Footer from "./components/footer/Footer";
 import SectionImg1 from './components/sectionImg/SectionImg1';
 import SectionImg2 from "./components/sectionImg/SectionImg2";
 import SectionImg3 from "./components/sectionImg/SectionImg3"
 
-const App = () => {
-    return (
-      <>
+function componentsToMerge() {
+  return (
+    <>
         <SectionImg1 />
         <SectionImg2 />
         <SectionImg3/>
         <Footer />
       </>
-    );
-};
+  )
+}
 
-export default App;
+export default componentsToMerge
