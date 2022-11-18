9d437561c9b2decea834c3c4e75b15877d255712
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-11-18 15:00:19 -0300

    pages folder maded, organizated app.js

--- frontend/cabininn/src/App.js
+++ frontend/cabininn/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/navbar/Navbar';
+import Nav from './components/navbar/NavBarAll';
+import Home from './pages/home/Home'
 import Footer from "./components/footer/Footer";
-import Carrousel from './components/carousel/Carrousel';
-import SectionImg1 from './components/sectionImg/SectionImg1';
-import SectionImg2 from "./components/sectionImg/SectionImg2";
-import SectionImg3 from "./components/sectionImg/SectionImg3"
 
 const App = () => {
     return (
       <>
         <Nav />
-        <Carrousel />
-        <SectionImg1 />
-        <SectionImg2 />
-        <SectionImg3/>
+        <Home/>
         <Footer />
       </>
     );
