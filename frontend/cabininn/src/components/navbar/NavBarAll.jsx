c018eea42df94b5261948449c587257f2ed3972b
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-11-20 19:36:45 -0300

    new search maded in results page

--- frontend/cabininn/src/components/navbar/NavBarAll.jsx
+++ frontend/cabininn/src/components/navbar/NavBarAll.jsx
@@ -3,30 +3,35 @@ import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Login from "../../pages/login/Login";
+import {
+    NavLink,
+} from 'react-router-dom'
 
 const NavBarAll = ({setModalShow, modalShow}) => {
 
 
 
   return (
+    <>
     <div>
       <Navbar bg="light">
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="/">
             <h2>CabinInn</h2>
           </Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-end">
-            <Button variant="secondary" className="mx-3">
-              Boton
-            </Button>
+            <NavLink to="/results" variant="secondary" className="btn btn-secondary mx-2">
+              Buscar Hoteles
+            </NavLink>
             <Button variant="secondary" onClick={()=>setModalShow(true)}>Login</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Login show={modalShow}
-        onHide={() => setModalShow(false)}/>
+        onHide={() => setModalShow(false)}/>    
     </div>
+    </>
   );
 };
 
--- frontend/cabininn/src/pages/results/Results.jsx
+++ frontend/cabininn/src/pages/results/Results.jsx
@@ -1,11 +1,115 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+import "./results.css";
 
 const Results = () => {
-    return (
-        <div>
-            Results
+  const [search, setSearch] = useState({
+    query: "",
+    list: [],
+  });
+
+  const posts = [
+    {
+      url: "",
+      tags: ["Hotel", "vacaciones", "hoteles", "5 estrellas"],
+      title: "Hotel de ejemplo",
+    },
+    {
+      url: "",
+      tags: ["node", "express"],
+      title: "Hotel de ejemplo 2",
+    },
+    {
+      url: "",
+      tags: ["node", "express"],
+      title: "Argentina",
+    },
+  ];
+
+  const handleChange = (e) => {
+    const results = posts.filter((post) => {
+      if (e.target.value === "") return posts;
+      return post.title.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setSearch({
+      query: e.target.value,
+      list: results,
+    });
+  };
+
+  return (
+    <>
+      <form>
+        <div className="containers">
+          <input
+            placeholder="Type to search..."
+            className="inputs"
+            name="text"
+            type="search"
+            value={search.query}
+            onChange={handleChange}
+          />
+          <div className="icons">
+            <svg
+              viewBox="0 0 512 512"
+              className="ionicon"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Search</title>
+              <path
+                strokeWidth="32"
+                strokeMiterlimit="10"
+                stroke="currentColor"
+                fill="none"
+                d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
+              ></path>
+              <path
+                d="M338.29 338.29L448 448"
+                strokeWidth="32"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                stroke="currentColor"
+                fill="none"
+              ></path>
+            </svg>
+          </div>
         </div>
-    );
+      </form>
+      <Container>
+      <h2>Buscar Hoteles</h2>
+      <div>
+        {search.query === ""
+          ? `Busqueda vacia`
+          : !search.list.length
+          ? "No se encontraron resultados"
+          : search.list.map((post) => {
+              return (
+                <>
+                  <Row xs={2} md={3} className="g-3 mt-2">
+                    {posts.map((post) => (
+                      <Col>
+                        <Card className="text-center" border="success">
+                          <Card.Img variant="top" src="" />
+                          <Card.Body className="py-4 my-4">
+                            <Card.Title>{post.title}</Card.Title>
+                            <Card.Text>Example text</Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    ))}   
+                  </Row>
+                </>
+              );
+            })}
+            </div>
+      </Container>
+    </>
+  );
 };
 
 export default Results;
