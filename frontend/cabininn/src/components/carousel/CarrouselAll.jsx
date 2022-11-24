4091c65424a778f88cc2b2e0a52fe82b21e2e960
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-11-24 07:57:05 -0500

    carrousel terminado

--- frontend/cabininn/src/components/carousel/CarrouselAll.jsx
+++ frontend/cabininn/src/components/carousel/CarrouselAll.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./carrousel.css";
-import { motion } from "framer-motion"
-import image1 from '../../images/Barilocheuikit.jpg'
-import image2 from '../../images/UshuaiaUIKIT.jpg'
-import image3 from '../../images/CarlosPazuikit.jpg'
+import image1 from "../../images/Barilocheuikit.jpg";
+import image2 from "../../images/UshuaiaUIKIT.jpg";
+import image3 from "../../images/CarlosPazuikit.jpg";
+import { Link } from "react-router-dom";
 
-const images = [image1, image2, image3]
+const images = [image1, image2, image3];
 
 const CarrouselAll = () => {
   const [index, setIndex] = useState(0);
@@ -15,18 +15,25 @@ const CarrouselAll = () => {
     setIndex(selectedIndex);
   };
   return (
-    <div className="carrousel-section">
+    <div className="carrousel-section mb-5">
       <h2 className="text-center mt-5 mb-2 tittle-carr">
         Promociones Noviembre
       </h2>
-      <p className="content-carr">
+      <p className="content-carr mb-3">
         ¿Quién dijo paz? Aprovechá estas ofertas y disfrutá un fin de semana en
         el bosque.
       </p>
-
-     <motion.h1 animate={{x:200}}>Hola mundo</motion.h1>
-
-
+      <div className="container-img d-flex justify-content-center gap-4">
+        <button className="btn-slider fs-4"><i className="fa-solid fa-chevron-left"></i></button>
+        {images.map((img, id) => (         
+            <div key={id}>
+            <Link to={`/reservation/${id+1}`}>
+            <img className="item-img" src={img} alt="" />
+            </Link>
+            </div>
+        ))}
+        <button className="btn-slider fs-4"><i className="fa-solid fa-chevron-right"></i></button>
+      </div>
     </div>
   );
 };
