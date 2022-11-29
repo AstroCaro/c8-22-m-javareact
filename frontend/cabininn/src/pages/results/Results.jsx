ade9ac0496508ad50210743a7da5dd470701b5e1
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-11-29 08:15:07 -0300

    cards printed from apiBack.json

--- frontend/cabininn/src/pages/results/Results.jsx
+++ frontend/cabininn/src/pages/results/Results.jsx
@@ -1,10 +1,10 @@
-import React, { useMemo } from "react";
-import { useState, useRef } from "react";
+import React from "react";
+// import { useState, useRef, useMemo } from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import API from "./api.json";
+import API from "./apiBack.json";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BiCommentDetail } from "react-icons/bi";
 import { MdGTranslate } from "react-icons/md";
@@ -12,37 +12,37 @@ import { MdGTranslate } from "react-icons/md";
 import "./results.css";
 
 const Results = () => {
-  const [query, setQuery] = useState("");
-  const [items, setItems] = useState([]);
+  // const [query, setQuery] = useState("");
+  // const [items, setItems] = useState([]);
 
-  const inputRef = useRef();
-  console.log(items);
+  // const inputRef = useRef();
+  // console.log(items);
 
   const hoteles = API;
 
-  const filteredHoteles = useMemo(() => {
-    return hoteles.filter((items) =>
-      items.name.toLowerCase().includes(query.toLowerCase())
-    );
-  }, [query, hoteles]);
+  // const filteredHoteles = useMemo(() => {
+  //   return hoteles.filter((items) =>
+  //     items.name.toLowerCase().includes(query.toLowerCase())
+  //   );
+  // }, [query, hoteles]);
 
-  function onSubmit(e) {
-    e.preventDefault();
+  // function onSubmit(e) {
+  //   e.preventDefault();
 
-    const value = inputRef.current.value;
+  //   const value = inputRef.current.value;
 
-    setQuery(value);
-    if (value === "") return;
-    setItems((prev) => {
-      return [...prev, value];
-    });
+  //   setQuery(value);
+  //   if (value === "") return;
+  //   setItems((prev) => {
+  //     return [...prev, value];
+  //   });
 
-    inputRef.current.value = "";
-  }
+  //   inputRef.current.value = "";
+  // }
 
   return (
     <>
-      <form onSubmit={onSubmit}>
+      {/* <form onSubmit={onSubmit}>
         <div className="containers">
           <input
             placeholder="Type to search..."
@@ -77,9 +77,11 @@ const Results = () => {
             </svg>
           </div>
         </div>
-      </form>
+      </form> */}
       <Container>
-        <h2 className="mainTitleResult text-center mt-5">7 Resultados para Bariloche</h2>
+        <h2 className="mainTitleResult text-center ">
+          7 Resultados para Bariloche
+        </h2>
         {/* <h2>agregar Hoteles</h2>
         <form onSubmit={onSubmit}>
           Nuevo Hotel
@@ -89,57 +91,75 @@ const Results = () => {
           </button>
         </form> */}
         <div className="text-center">
-          <h2 className="subTitleResult">Estos son los resultados de tu búsqueda ¡Encontrá el que más te gusté! </h2>
+          <h2 className="subTitleResult">
+            Estos son los resultados de tu búsqueda ¡Encontrá el que más te
+            gusté!{" "}
+          </h2>
           <>
             <Row xs={1} md={2} className="g-3 mt-2">
-              {filteredHoteles
+              {/* {filteredHoteles
                 .filter((item) => {
                   return item.title.toLowerCase().includes(query.toLowerCase());
-                })
-                .map((item) => (
-                  <Col>
-                    <Card className="shadow-lg my-4 mx-2" border="success">
-                      <Card.Img
-                        variant="top"
-                        className="img-fluid h-100 rounded"
-                        src={item.urlFoto}
-                      />
-                      <Card.Body className="py-4 my-2 border-3">
-                        <Row>
-                          <Col className="text-start mx-4">
-                            <Card.Title>
-                              <span className="cardTitleResult">
-                                {item.title}
-                              </span>
-                            </Card.Title>
-                          </Col>
-                          <Col className="text-end">
-                            <span className="cardIcons mx-2 my-4">
-                              <BiCommentDetail />
-                            </span>
-                            <span className="cardIcons mx-2 my-4">
-                              <AiOutlineHeart />
-                            </span>
-                            <span className="cardIcons mx-2 my-4">
-                              <MdGTranslate />
-                            </span>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col className="text-start mx-4">
-                            <Card.Text>descripcion : {item.name}</Card.Text>
-                            <Card.Text>Reservacion : {item.country}</Card.Text>
-                            <Card.Text>Tags : {item.tags.join(", ")}</Card.Text>
-                            <Card.Text className="cardTotal">Total : $ {item.price}</Card.Text>
-                          </Col>
-                          <Col className="text-end my-2">
-                            <span className="btn btnCard">Reservar</span>
-                          </Col>
-                        </Row>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))}
+                }) */}
+              {hoteles.map((item) => (
+                <Col>
+                  <Card className="shadow-lg my-4 mx-2" border="success">
+                    <Card.Img
+                      variant="top"
+                      className="img-fluid h-100 rounded"
+                      src={item.urlFoto}
+                    />
+                    <Card.Body className="py-4 my-2 border-3">
+                      <Row>
+                        <Col className="text-start mx-4">
+                          <Card.Title>
+                            <span className="cardTitleResult">{item.name}</span>
+                          </Card.Title>
+                        </Col>
+                        <Col className="text-end">
+                          <span className="cardIcons mx-2 my-4">
+                            <BiCommentDetail />
+                          </span>
+                          <span className="cardIcons mx-2 my-4">
+                            <AiOutlineHeart />
+                          </span>
+                          <span className="cardIcons mx-2 my-4">
+                            <MdGTranslate />
+                          </span>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col className="text-start mx-4">
+                          <Card.Text>descripcion : {item.name}</Card.Text>
+                          <Card.Text>chainCode : {item.chainCode}</Card.Text>
+                          <Card.Text>iataCode : {item.iataCode}</Card.Text>
+                          <Card.Text>
+                            address :{item.address.postalCode}
+                            <br />
+                            {item.address.countryCode}
+                            <br />
+                            {item.address.countryName}
+                            <br />
+                            {item.address.cityName}
+                            <br />
+                            {item.address.id}
+                          </Card.Text>
+                          <Card.Text>
+                            distance :{item.distance.value}
+                            {item.distance.unit}
+                          </Card.Text>
+                          <Card.Text className="cardTotal">
+                            Total : $ {item.dailyPrice}
+                          </Card.Text>
+                        </Col>
+                        <Col className="text-end my-2">
+                          <span className="btn btnCard">Reservar</span>
+                        </Col>
+                      </Row>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
             </Row>
           </>
         </div>
