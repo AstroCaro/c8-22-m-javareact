f286e3c1ac8858cb52fe505461f972338821baf2
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-11-29 07:50:32 -0500

    prueba axios

--- frontend/cabininn/src/components/navbar/NavBarAll.jsx
+++ frontend/cabininn/src/components/navbar/NavBarAll.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,15 +7,44 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logoSVG from "./logo.svg";
 import InputSearch from "../header/InputSearch";
+import axios from "axios";
 
 const NavBarAll = ({ setModalShow, modalShow }) => {
   const [colore, setcolore] = useState(false);
+  const [searching, setsearching] = useState('');
+  const [capture, setcapture] = useState('Argentina')
 
   const changecolor = () => {
     setcolore(true);
   };
 
 window.addEventListener('scroll', changecolor)
+//  const data = JSON.stringify({countryName: capture })
+
+
+// useEffect(()=> {
+
+ 
+// },[])
+
+const search = () => {
+setcapture(searching)
+// axios.post('https://pruebanocountry-production.up.railway.app/list/',
+// {"countryName": "Peru"})
+const config = {
+  mode: 'no-cors',
+  headers:{
+    'Access-Control-Allow-Origin': '*',
+       
+  },
+  withCredentials: true,
+  credentials: 'same-origin',
+};
+axios.get('http://pruebanocountry-production.up.railway.app/hotels', config)
+
+.then( res => console.log(res.data))
+.catch( err => console.log(err))
+}
 
   return (
     <>
@@ -26,6 +55,7 @@ window.addEventListener('scroll', changecolor)
         <nav className="me-5">
           <ul>
             <li>
+              <input type="text" onChange={(e)=>setsearching(e.target.value)} />
               <a
                 className="btn-reg rounded"
                 href="#"
@@ -44,7 +74,7 @@ window.addEventListener('scroll', changecolor)
               </a>
             </li>
             <li>
-              <a className="btn-search fs-2 ms-5" href="#">
+              <a className="btn-search fs-2 ms-5" href="#" onClick={search}>
                 <i className="fa-solid fa-magnifying-glass"></i>
               </a>
             </li>
