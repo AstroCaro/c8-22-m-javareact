75eb668c8857d141d0751b449a0c1718d4b396b3
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-01 10:02:08 -0300

    stylization in searchbox

--- frontend/cabininn/src/components/navbar/NavBarAll.jsx
+++ frontend/cabininn/src/components/navbar/NavBarAll.jsx
@@ -1,48 +1,44 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
+import React, {useState } from "react";
 import Login from "../../pages/login/Login";
-import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logoSVG from "./logo.svg";
-import InputSearch from "../header/InputSearch";
-import axios from "axios";
 import { Link } from "react-router-dom";
-const NavBarAll = ({ setModalShow, modalShow, setsearching, search, searching }) => {
+const NavBarAll = ({
+  setModalShow,
+  modalShow,
+  setsearching,
+  search,
+  searching,
+}) => {
   const [colore, setcolore] = useState(false);
-  // const [searching, setsearching] = useState('');
-  // const [capture, setcapture] = useState('Argentina')
-  // const [hotels, sethotels] = useState([])
-
 
   const changecolor = () => {
     setcolore(true);
   };
 
-window.addEventListener('scroll', changecolor)
-
-
-// const search = (bycountry) => {
-// setcapture(searching)
-// axios.get('https://api.jsonbin.io/v3/b/638601547966e84526cf3d75')
-// .then( res => sethotels(res.data.record))
-// .catch( err => console.log(err))
-
-// const hotelbyCountry = hotels.filter( hotel => hotel.address.countryName == 'Argentina' )
-
-// }
+  window.addEventListener("scroll", changecolor);
 
   return (
     <>
-      <header className={colore ? 'bg-header' : ''}>
+      <header className={colore ? "bg-header" : ""}>
         <a className="ms-5" href="/">
-          <img src={logoSVG} alt="" />
+          <img src={logoSVG} alt="logo" className="logo" />
         </a>
         <nav className="me-5">
           <ul>
             <li>
-              <input type="text" onChange={(e)=>setsearching(e.target.value)} value={searching} />
+              <input
+                list="countries"
+                type="text"
+                onChange={(e) => setsearching(e.target.value)}
+                value={searching}
+                className="searchBox"
+                placeholder=" Buscar"
+              />
+              <datalist id="countries">
+                <option value="Argentina"></option>
+                <option value="Perú"></option>
+              </datalist>
               <a
                 className="btn-reg rounded"
                 href="#"
@@ -61,7 +57,11 @@ window.addEventListener('scroll', changecolor)
               </a>
             </li>
             <li>
-              <Link className="btn-search fs-2 ms-5" to={`./results/${searching}`} onClick={search}>
+              <Link
+                className="btn-search fs-2 ms-5"
+                to={`./results/${searching}`}
+                onClick={search}
+              >
                 <i className="fa-solid fa-magnifying-glass"></i>
               </Link>
             </li>
