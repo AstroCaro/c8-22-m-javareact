a00a1d9ebcc7f1fccd82fe3deab1bba1464eaafe
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-12-07 09:40:55 -0500

    cambios hacia el responsive

--- frontend/cabininn/src/components/header/HeaderAll.jsx
+++ frontend/cabininn/src/components/header/HeaderAll.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BtnWatsap from "./BtnWatsap";
-
+import '../navbar/Navbar.css'
 import "./header.css";
 import backgroundImg from './backgroundImg.jpg'
 import Card from "react-bootstrap/Card";
@@ -8,11 +8,22 @@ import Card from "react-bootstrap/Card";
 const HeaderAll = () => {
   return (
     <>
+     <form action="" className="form-input d-sm-none">
+           <input
+            list="countries"
+            type="text"
+            // onChange={(e) => setsearching(e.target.value)}
+            // value={searching}
+            className="searchBox px-3 w-100 mx-3 "
+            placeholder=" Buscar"
+          />
+          </form>
       <Card className="heroCard bg-dark">
         {/* <Card.Img 
         src={backgroundImg}
          className="heroImg" 
          alt="Card image" /> */}
+        
         <Card.ImgOverlay>
           <Card.Title className="heroTitle flex text-center py-5 text-white">
             Encontra tu alojamiento ideal
